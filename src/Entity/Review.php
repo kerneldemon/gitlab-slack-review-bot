afd15f248f895927b0ec3d3c33ca30b4d2c07b1b
Dima <dima@nordsec.com>
2021-05-18 15:17:29 +0300

    No loop

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -47,7 +47,7 @@ class Review
     private $status;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $scope;
 
