cb0d60d68fb52757c57d0dea3d11737271bb5127
Dima <dima@nordsec.com>
2021-05-19 18:20:45 +0300

    Re-add migrations

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,6 +6,8 @@ export TERM=xterm
 /usr/local/bin/docker-entrypoint.sh mysqld &
 /etc/init.d/nginx start &
 
+screen -dmS 'doctrine-database-create' bash -c 'until php /var/www/bin/console --no-interaction doctrine:database:create; do echo Unsuccessful; done'
+screen -dmS 'doctrine-migrations-migrate' bash -c 'until php /var/www/bin/console --no-interaction doctrine:migrations:migrate; do echo Unsuccessful; done'
 screen -dmS 'blacklist-sync' watch -n 1800 php /var/www/bin/console app:blacklist:sync
 
 watch -n 1 php /var/www/bin/console app:project:setup
